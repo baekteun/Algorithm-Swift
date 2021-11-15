c5eecaa7f977b0efc12be763d09ea9437ae3b76a
baekteun <baegteun@gmail.com>
2021-11-15 18:18:27 +0900

    6. 봉우리
    **지도 정보가 N*N 격자판에 주어집니다. 각 격자에는 그 지역의 높이가 쓰여있습니다. 각 격자**
    
    **판의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자는 봉우리 지역입니다. 봉우리 지역이 몇 개**
    
    **있는 지 알아내는 프로그램을 작성하세요.**
    
    **격자의 가장자리는 0으로 초기화 되었다고 가정한다.**
    
    **만약 N=5 이고, 격자판의 숫자가 다음과 같다면 봉우리의 개수는 10개입니다.**
    
    → 상,하,좌,우가 모두 자신보다 작다면 봉우리가 된다.
    
    **▣입력설명**
    
    **첫 줄에 자연수 N이 주어진다.(1<=N<=50)**
    
    **두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는**
    
    **다.**
    
    **▣ 출력설명**
    
    **봉우리의 개수를 출력하세요.**
    
    **▣ 입력예제 1**
    
    **5**
    
    **5 3 7 2 3**
    
    **3 7 1 6 1**
    
    **7 2 5 3 4**
    
    **4 3 6 4 1**
    
    **8 7 3 5 2**
    
    **▣ 출력예제 1**
    
    **10**

---
+++ Algorithm-Swift/peaks/peaks.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  Algorithm-Swift
+//
+//  Created by baegteun on 2021/11/15.
+//
+
+import Foundation
+let t = Int(readLine()!)!
+var input = [[Int]]()
+for _ in 0..<t{
+    input.append(readLine()!.split(separator: " ").map{Int($0)!})
+}
+let dx = [1, 0, -1, 0]
+let dy = [0, 1, 0, -1]
+var res = 0
+var isPeaks = [[Bool]]()
+for _ in 0..<t{isPeaks.append(Array(repeating: false, count: t))}
+
+for i in 0..<t{
+    for k in 0..<t{
+        if isPeaks[i][k]{
+            continue
+        }
+        var flag = true
+        for m in 0..<4{
+            let nx = i + dx[m]
+            let ny = k + dy[m]
+            if nx >= t || ny >= t || nx < 0 || ny < 0{
+                continue
+            }
+            if input[i][k] <= input[nx][ny]{
+                flag = false
+                break
+            }
+            
+        }
+        if flag{
+            res += 1
+            isPeaks[i][k] = true
+        }
+    }
+}
+print(res)
