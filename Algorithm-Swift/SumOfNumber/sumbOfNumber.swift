a6694e66017ced3f990cf4f4e3415a86b786d515
baekteun <baegteun@gmail.com>
2021-11-15 17:29:29 +0900

    4. 수의 합
    **5*5 격자판에 아래롸 같이 숫자가 적혀있습니다.**
    
    **N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선의 합 중 가 장 큰 합을 출력합**
    
    **니다.**
    
    ![Untitled](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/b66b34d8-c390-41a9-818b-24e4bc3c8620/Untitled.png)
    
    **▣ 입력설명**
    
    **첫 줄에 자연수 N이 주어진다.(1<=N<=50) ← 몇 행인지**
    
    **두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는**
    
    **다.**
    
    **▣ 출력설명**
    
    **최대합을 출력합니다.**
    
    **▣ 입력예제 1**
    
    **5**
    
    **10 13 10 12 15**
    
    **12 39 30 23 11**
    
    **11 25 50 53 15**
    
    **19 27 29 37 27**
    
    **19 13 30 13 19**
    
    **▣ 출력예제 1**
    
    **155**

---
+++ Algorithm-Swift/SumOfNumber/sumbOfNumber.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  Algorithm-Swift
+//
+//  Created by baegteun on 2021/11/15.
+//
+
+import Foundation
+var res: [Int] = []
+let t = Int(readLine()!)!
+var input = [[Int]]()
+for _ in 0..<t{
+    input.append(readLine()!.split(separator: " ").map{Int($0)!})
+}
+var Max = Int.min
+
+for i in 0..<t{
+    var col = 0
+    for k in 0..<t{
+        col += input[k][i]
+    }
+    Max = max(col, input[i].reduce(0, +), Max)
+}
+var s1 = 0
+var s2 = 0
+for i in 0..<t{
+    s1 += input[i][i]
+    s2 += input[i][t-i-1]
+}
+Max = max(s1, s2, Max)
+print(Max)
